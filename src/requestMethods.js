cf17e4d117bdf55638ec8312451d52b7f25e42b1
shivCan99 <107194536+shivCan99@users.noreply.github.com>
2023-03-03 11:33:15 -0500

    Update requestMethods.js

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const BASE_URL = "http://localhost:5050/api/";
-const TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYzZmE0YzkzNWFiMGUxMDY1NjBhMzIwNCIsImlzQWRtaW4iOnRydWUsImlhdCI6MTY3NzM2OTkzNiwiZXhwIjoxNjc3NjI5MTM2fQ.s7dNr0hFCCencq7Ru2tbyZ3Bz2zPmews0D1MpKivSYA";
+const TOKEN = "yourToken";
 
 export const publicRequest = axios.create({
     baseURL:BASE_URL,
